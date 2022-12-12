5fa368ca74312150a58e84508e90769d01a71298
Colton <coltonbitz@gmail.com>
2022-12-12 15:18:08 -0700

    Basic project setup

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import '../styles/globals.css';
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <>
+    {/* Nav */}
+    <p className='text-xl'>Home</p>
+    {/* Hero */}
+
+    {/* About Me */}
+
+    {/* Skills */}
+      
+    {/* Projects */}
+
+    {/* Contact Me */}
+    </>
+  )
+}
+
+export default Home
