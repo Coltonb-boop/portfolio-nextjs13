aee999f2bf11f0da819b44116f957d186bb75a09
Colton <coltonbitz@gmail.com>
2022-12-16 09:37:26 -0700

    Completed all base features
    Moving on to adding database

--- app/ContactMe.tsx
+++ app/ContactMe.tsx
@@ -1,69 +1,96 @@
-import React from 'react'
-import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+'use client'
+import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  name: string,
-  email: string,
-  subject: string,
-  message: string,
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
 };
 
-type Props = {}
+type Props = {};
 
 function ContactMe({}: Props) {
-  const { 
-    register, 
-    handleSubmit,
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = formData => console.log(formData);
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:coltonbitz@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message}`  
+  };
 
   return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl mx-auto justify-evenly items-center px-10'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl mx-auto justify-evenly items-center px-10">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Contact
       </h3>
-      
-      <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center'>
-          Sentence goes here. {' '}
-          <span className='decoration-[#F7AB0A]/50 underline'>There it is.</span>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          Sentence goes here.{" "}
+          <span className="decoration-[#F7AB0A]/50 underline">
+            There it is.
+          </span>
         </h4>
 
-        <div className='space-y-10'>
-          <div className='flex items-center space-x-5 justify-center'>
-            <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>+1234556789</p>
-          </div>
-          
-          <div className='flex items-center space-x-5 justify-center'>
-            <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>myemail@gmail.com</p>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+1234556789</p>
           </div>
 
-          <div className='flex items-center space-x-5 justify-center'>
-            <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse' />
-            <p className='text-2xl'>123 Developer Lane</p>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">myemail@gmail.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">123 Developer Lane</p>
           </div>
         </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col w-fit mx-auto space-y-2'>
-          <div className='flex space-x-2'>
-            <input {...register('name')} placeholder='Name' className='contactInput' type="text" />
-            <input {...register('email')} placeholder='Email' className='contactInput' type="email" />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col w-fit mx-auto space-y-2"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
           </div>
 
-          <input placeholder='Subject' className='contactInput' type="text" />
-          
-          <textarea placeholder='Message' className='contactInput' />
-          <button 
-            type='submit' 
-            className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold'>Submit
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold"
+          >
+            Submit
           </button>
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default ContactMe
+export default ContactMe;
--- app/ExperienceCard.tsx
+++ app/ExperienceCard.tsx
@@ -6,13 +6,13 @@ type Props = {}
 
 function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+        className='w-32 h-32 rounded-full xl:w-[150px] xl:h-[150px] object-cover object-center'
         src='profilepic.jpg'
         alt=''
       />
