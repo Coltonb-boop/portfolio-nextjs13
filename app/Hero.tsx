5ab62860eada7197595f8690a33a9745eccd3c74
Colton <coltonbitz@gmail.com>
2022-12-20 16:58:39 -0700

    Adding responsiveness to hero

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -16,7 +16,7 @@ function Hero({}: Props) {
   });
   
   return (
-    <div className='flex items-center justify-evenly text-center overflow-hidden bg-indigo-50 m-16 mt-0 h-[500px] rounded-md'>
+    <div className='flex items-center justify-evenly text-center overflow-hidden bg-indigo-50 m-16 mt-0 h-[500px] rounded-md md:max-w-4xl md:mx-auto'>
       <div className='flex flex-col items-center justify-center space-y-8 pl-10 max-w-md text-left'>
         <div className='text-4xl font-bold'>
           <p>Building digital </p>
@@ -27,7 +27,7 @@ function Hero({}: Props) {
           <p>Full-stack Web Developer and Experience Curator</p>
           <p>I favor using NextJS and React to create a modern look and feel.</p>
         </div>
-        <button className='bg-purple-700 w-40 h-14 text-white'>
+        <button className='bg-purple-700 hover:bg-purple-500 w-40 h-14 text-white'>
           Contact Me
         </button>
       </div>
