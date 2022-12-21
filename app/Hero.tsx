836607e9b0ddcca0b64feecd03d3be5a55f6ba65
Colton <coltonbitz@gmail.com>
2022-12-21 16:58:45 -0700

    Got transparent background in
    Had to use base css

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,25 +1,12 @@
 'use client'
 import React from 'react'
-import { Cursor, useTypewriter } from 'react-simple-typewriter'
-import BackgroundCircles from './BackgroundCircles';
 
 type Props = {}
 
 function Hero({}: Props) {
-  const [text, count] = useTypewriter({
-    words: [
-      'Hi, I\'m Colton',
-      'And I love to code'
-    ],
-    loop: true,
-    delaySpeed: 2000,
-  });
   
   return (
-    <div className='relative flex items-center justify-evenly text-center bg-hero-pattern z-10 bg-indigo-50 m-16 mt-0 h-[500px] rounded-md md:max-w-4xl md:mx-auto'>
-      <div className='absolute bg-hero-pattern z-20 w-full h-full'>
-
-      </div>
+    <div className='hero-background relative flex items-center justify-evenly text-center bg-hero-pattern z-10 bg-indigo-50 m-16 mt-0 h-[500px] md:rounded-md w-full md:max-w-4xl mx-auto'>
       <div className='flex flex-col items-center justify-center space-y-8 pl-10 max-w-md text-left'>
         <div className='text-4xl font-bold'>
           <p>Building digital </p>
@@ -40,28 +27,6 @@ function Hero({}: Props) {
         src='profilepic.jpg' 
         alt='' 
       />
-      {/* <div className='z-20'>
-        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[12px]'>Full Stack Developer</h2>
-        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
-          <span className='mr-3'>{text}</span>
-          <Cursor cursorColor='#F7AB0A' />
-        </h1>
-
-        <div className='pt-5'>
-          <a href="#about" >
-            <button className='heroButton'>About</button>
-          </a>
-          <a href='#experience'>
-            <button className='heroButton'>Experience</button>
-          </a>
-          <a href='#skills'>
-            <button className='heroButton'>Skills</button>
-          </a>
-          <a href='#projects'>
-            <button className='heroButton'>Projects</button>
-          </a>
-        </div>
-      </div> */}
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ const Home = (props: Props) => {
       </section>
       
       <a href='#top'>
-        <footer className='sticky bottom-5 cursor-pointer'>
+        <footer className='sticky bottom-5 cursor-pointer hover:bg-purple-500'>
           <div className='flex items-center justify-center'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
