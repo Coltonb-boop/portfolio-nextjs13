edf08b7fe8366b719cc1492224779b6af11f70b5
Colton <coltonbitz@gmail.com>
2023-01-03 12:18:57 -0700

    Fixed header, to-top button
    Header has slight transparency for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -12,17 +12,15 @@ type Props = {}
 const Home = (props: Props) => {
   
   return (
-    <div id='top' className='bg-[#fff] text-black h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 min-w-[640]'>
+    <div className='bg-[#fff] text-black h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 min-w-[640]'>
       <Header />
       
       <a href='#top' className=''>
-        <footer className='absolute right-5 bottom-5 cursor-pointer z-30'>
+        <footer className='absolute right-10 bottom-5 cursor-pointer z-30'>
           <div className='flex items-center justify-center'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-10 w-10 bg-white rounded-full hover:text-purple-700">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-
-            
           </div>
         </footer>
       </a>
