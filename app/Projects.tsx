9d7760d43cec1871fb01537610937c14b5092953
Colton <coltonbitz@gmail.com>
2023-01-10 14:10:07 -0700

    Skills revamp complete
    Moving onto projects section

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -13,13 +13,13 @@ function Projects({}: Props) {
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
       className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='uppercase text-gray-500 text-2xl'>
         Projects
       </h3>
 
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
         {projects.map((project, index) => (
-          <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+          <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col items-center justify-center h-screen'>
             <motion.img 
               initial={{ y: -300, opacity: 0 }}
               transition={{ duration: 1.2 }}
--- todo.md
+++ todo.md
@@ -1,3 +1,10 @@
+** Header / Nav
+* Header/nav maybe split into 2 rows when compressed
+- or -
+* Clicking 'hamburger' swaps between socials and nav
+
+** Skills
 * Make highlight color the same purple as on the to-top-page button
 * Skills section add highlight
 * Skills might require better spacing / spicing up
+* Maybe grow on hover instead of spin
