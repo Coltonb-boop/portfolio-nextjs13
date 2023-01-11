fdc8467fc02840f4a49135600c6262d6a9fecf6f
Colton <coltonbitz@gmail.com>
2023-01-10 17:35:42 -0700

    Setup initial settings for carousel

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -1,27 +1,57 @@
 'use client'
 import { motion } from 'framer-motion';
-import React from 'react'
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 type Props = {}
 
 function Projects({}: Props) {
   const projects = [1,2,3,4,5];
+  /* 
+  [
+    1: { 'title': ''}
+  ]
+  */
   
   return (
     <motion.div
       initial={{ opacity: 0 }}
       transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
-      className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'
+      className='h-screen flex relative flex-col text-center justify-center items-center md:text-left xl:flex-row max-w-[800px] min-h-screen mx-auto'
     >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
 
-      <div className="grid grid-cols-4 gap-5">
-        {/* Map through projects here */}
+      {/* React Carousel */}
+      <div className='w-full'>
+        <Carousel autoPlay interval={3000} infiniteLoop showThumbs={false} stopOnHover showStatus={false}>
+          {projects.map((project, index) => {
+            return (
+              <div key={index} className='bg-green-400 h-[500px]'>
+                <img src='' />
+                <p className='text-lg font-semibold'>My Favorite Project</p>
+                <p>{project}</p>
+              </div>
+            )
+          })}
+        </Carousel>
       </div>
 
+      {/* Mapping method */}
+      {/* <div className="grid grid-cols-2 gap-5">
+        {projects.map((project, index) => {
+          return (
+            <div key={index} className='bg-green-400 w-48 h-40'>
+              {project}
+            </div>
+          )
+        })}
+      </div> */}
+
+      {/* This was a fancy background line */}
       {/* <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12' /> */}
     </motion.div>
   )
