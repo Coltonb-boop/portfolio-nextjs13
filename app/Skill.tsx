3c0f16e7fd06ddb940dfa278f7e112bc923982e6
Colton <coltonbitz@gmail.com>
2023-01-23 11:11:26 -0700

    skills updated to new transition

--- app/Skill.tsx
+++ app/Skill.tsx
@@ -9,23 +9,18 @@ type Props = {
 
 function Skill({ url, even }: Props) {
   return (
-    <div className={`group relative flex cursor-pointer origin-center hover:scale-125 transition duration-700`}>
+    <div className={`group relative flex origin-center hover:scale-125 transition duration-700`}>
       <motion.img 
         initial={{
-          x: true ? -100 : 100,
+          y: 100,
           opacity: 0
         }}
         src={url}
-        transition={{ duration: 1 }}
-        whileInView={{ opacity: 1, x:0 }}
+        transition={{ duration: .5 }}
+        whileInView={{ opacity: 1, y:0 }}
         viewport={{ once: true }}
-        className={`w-24 h-24 lg:w-32 lg:h-32 filter group-hover:grayscale transition duration-300 ease-in-out p-2 hover:translate-y-10`}
+        className={`w-24 h-24 lg:w-32 lg:h-32 filter hover:grayscale transition duration-300 ease-in-out p-2 hover:translate-y-10`}
       />
-      {/* <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 xl:w-32 xl:h-32 z-0'>
-        <div className='flex items-center justify-center h-full'>
-          <p className='text-3xl font-bold text-black opacity-100'>100%</p>
-        </div>
-      </div> */}
     </div>
   )
 }
--- todo.md
+++ todo.md
@@ -2,18 +2,10 @@
 * Go over each section in detail
 * Double check responsiveness
 
-* play with motion(see below) > deploy
+* play with motion(see below)
 
 ** Motion
 * Goal is to have most things transitions to their spot from a -y and translucent, once it's in place, don't show again
-
-** Header / Nav
-* Header/nav maybe split into 2 rows when compressed
-- or -
-* Clicking 'hamburger' swaps between socials and nav
-
-** About
-* Move smiley to after from before
 
 ** Skills
 * Make highlight color the same purple as on the to-top-page button
